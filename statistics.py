a4063ab2b9a6a568b35676a59bd86e47f665da9c
Silvana <s.abdi.4@student.rug.nl>
2022-04-11 15:41:30 +0200

    Added last four functions and basic vars

--- statistics.py
+++ statistics.py
@@ -2,10 +2,21 @@ import re
 import wordfreq
 import nltk
 from wordfreq import word_frequency, tokenize
-#
-# with open('nos001.txt', 'r') as file:
-#     text = file.read()
 
+with open('/nos001.txt', 'r') as file:
+    text = file.read()
+
+# -------- BASIC VARIABLES ------------
+def sentences(text):
+    pass
+
+# check??
+def words(text):
+    return re.split("\s|(?<!\d)[,.](?!\d)", text)
+
+def characters():
+    words = words(sentences)
+    return words.split()
 
 # -------- WORD STATISTICS ------------
 def total_word_count(text):
@@ -66,18 +77,40 @@ def avg_word_count_per_sent():
 def avg_syll_count_per_sent():
     pass
 
-def longest_sentence():
-    pass
+# Silvana
+# test??
 
-def avg_char_count_per_sent():
-    pass
+def longest_sentence(text):
+    sentences = re.split("\s|(?<!\d)[,.](?!\d)", text)
+    longest_sent = ""
+    for sent in sentences:
+        if len(sent) > len(longest_sent):
+            longest_sent = sent
+    return longest_sent
+
+
+def avg_char_count_per_sent(text):
+    sentences = re.split("\s|(?<!\d)[,.](?!\d)", text)
+
+    char_count = 0
+    for sent in sentences:
+        char = sent.split()
+        char_count += len(char)
+
+    return (char_count / len(sentences))
 
 
 # ------------- FULL TEXT STATISTICS
-def total_characters_count():
-    pass
+def total_characters_count(text):
+    count = 0
+    for words in text:
+        for char in words:
+            count += 1
+    return count
 
 
-def total_syll_count():
-    pass
+def total_syll_count(text):
+    words = text.split()
+    return countsyllables_nlde(words) #import andreas' module
+    
 
