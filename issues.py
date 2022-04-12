70106b5e7cdb44c4abda3803f29ce3b78a5a9448
Tariq Ballout <tariqballout@Tariqs-MacBook-Pro.local>
2022-04-12 12:27:31 +0200

    added docstrings

--- issues.py
+++ issues.py
@@ -1,10 +1,12 @@
 import statistics
+from collections import Counter
 
 # This file is for analysing the text and providing the reader
 # with potential issues
 
 
 def language_checker(text):
+    """Checks if the language of the input is Dutch"""
     pass
 
 
@@ -12,6 +14,7 @@ def language_checker(text):
 
 
 def warning_long_word(text):
+    """Returns words that are longer than 12 characters"""
     words = []
     for word in text.split():
         if len(word) > 12:
@@ -20,17 +23,21 @@ def warning_long_word(text):
 
 
 def warning_difficult_word(text):
+    """Returns words that contain 5 syllables or more"""
     words = []
     for word in text.split():
-        if statistics.syll_count_word(word) >= 6:
+        if statistics.syll_count_word(word) >= 5:
             words.append(word)
     return words
 
 
 def warning_rare_word(text):
+    """Returns words that are rarely used in Dutch"""
+    words = []
     for word in text.split():
         if word == statistics.rare_words(word):
-            return word
+            words.append(word)
+        return words
 
 
 # -------------- POTENTIAL SENTENCE ISSUES --------------
@@ -48,8 +55,12 @@ def warning_passive_voice():
     pass  # kon alleen dingen met SpaCy vinden, dus moeten even kijken hoe belangrijk dit is.
 
 
-def repeat_after_each_other(text):
-    pass
+def repeating_words(text):
+    words = text.split()
+    word_counts = Counter(words)
+    for word, count in sorted(word_counts.items()):
+        if count >= 5:
+            print('"%s" is %d keer herhaald.' % (word, count))
 
 
 def first_word_of_sent_repeat():
